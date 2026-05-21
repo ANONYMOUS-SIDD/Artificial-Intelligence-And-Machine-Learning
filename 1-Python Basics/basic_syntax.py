565bfcac8e891746708fd91b8864a468f8e37409
ANONYMOUS--SIDD <siddhanttimalsina10@gmai.com>
2026-05-21 22:24:02 +0545

    Python Basic Syntax And Symantics

---
+++ 1-Python Basics/basic_syntax.py
@@ -0,0 +1,36 @@
+#Case Sensitivity - Python Is A Case Sensitive Language
+name="Siddhant"
+Name="Anonymous"
+print(name)
+print(Name)
+
+#Indentation
+age=32
+if age>18:
+    print("You Can Drive")
+
+#This Is A Single Line Comment
+'''This Is A 
+Multi Line Comment'''
+
+#Line Continuation
+sum=1+2+3+4+\
+5+6+7
+print(sum)
+
+#MultiStatements On Single Line
+x=20;y=40;z=x+y
+print(z)
+
+#Variable Declaration
+name="Siddhant Timalsina"
+num=10
+isFlag=True
+print(name)
+print(num)
+print(isFlag)
+
+#Type Intereference
+print(type(name))
+print(type(num))
+print(type(isFlag))
