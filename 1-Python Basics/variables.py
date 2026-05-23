7995226c93ceeb7785d74a4bbc1403850b1502b8
ANONYMOUS--SIDD <siddhanttimalsina10@gmai.com>
2026-05-24 00:24:07 +0545

    Variables In Python

---
+++ 1-Python Basics/variables.py
@@ -0,0 +1,23 @@
+#Variable Declaration
+num=5
+name="Siddhant Timalsina"
+age=20
+percentage=99.5
+is_engineer=True
+number="10"
+
+#Type Casting
+int(number)
+print(number)
+
+#Input Methods
+name=input("What Is Your Name? ")
+print(name)
+
+#The Input Methods Always Return String DataType 
+num1=input("Enter Your First Number ")
+num2=input("Enter Your Second Number ")
+sum=num1+num2
+#It Only Concatinate The Numbers
+print(sum)
+print(int(num1)+int(num2))
